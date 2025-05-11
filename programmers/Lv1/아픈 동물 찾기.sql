4bacb30953b06ba538536d4799b9b48114306fae
yerin <wbica148@gmail.com>
2025-05-11 18:49:43 +0900

    25.05.11.study

---
+++ programmers/Lv1/아픈 동물 찾기.sql
@@ -0,0 +1,8 @@
+-- 동물 보호소에 들어온 동물 중 아픈 동물의 아이디와 이름을 조회하는 SQL 문을 작성
+-- 이때 결과는 아이디 순으로 조회
+
+SELECT ANIMAL_INS.ANIMAL_ID,ANIMAL_INS.NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_INS.INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_INS.ANIMAL_ID
+;
