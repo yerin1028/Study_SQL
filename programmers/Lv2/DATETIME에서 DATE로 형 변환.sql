787e81cdcf8577b4d44b82c069fb1f28e79030a2
yerin <wbica148@gmail.com>
2025-05-21 23:08:26 +0900

    25.05.21.study

---
+++ programmers/Lv2/DATETIME에서 DATE로 형 변환.sql
@@ -0,0 +1,10 @@
+-- ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 
+-- 각 동물의 아이디와 이름, 들어온 날짜를 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 아이디 순으로 조회해야 합니다.
+
+SELECT ANIMAL_ID, NAME, TO_CHAR(DATETIME, 'YYYY-MM-DD') AS "날짜"
+
+FROM ANIMAL_INS 
+
+ORDER BY ANIMAL_ID
+;
