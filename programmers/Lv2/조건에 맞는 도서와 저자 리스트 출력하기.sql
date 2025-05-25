a10a507e53fb75d8e5b22e8037164edbd368090f
yerin <wbica148@gmail.com>
2025-05-25 21:15:43 +0900

    25.05.25.study

---
+++ programmers/Lv2/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,12 @@
+-- '경제' 카테고리에 속하는 도서들의 도서 ID(BOOK_ID), 저자명(AUTHOR_NAME), 출판일(PUBLISHED_DATE) 리스트를 출력하는 SQL문을 작성해주세요.
+-- 결과는 출판일을 기준으로 오름차순 정렬해주세요.
+
+SELECT B.BOOK_ID, A.AUTHOR_NAME, TO_CHAR(B.PUBLISHED_DATE, 'YYYY-MM-DD') AS PUBLISHED_DATE
+
+FROM BOOK B, AUTHOR A
+
+WHERE B.AUTHOR_ID = A.AUTHOR_ID
+AND B.CATEGORY = '경제'
+
+ORDER BY B.PUBLISHED_DATE 
+;
